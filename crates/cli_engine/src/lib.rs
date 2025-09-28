bdca13dfd7d9a72cd78832611546228a8adfc480
nikita-skobov <nskobov.websites@gmail.com>
2025-09-28 15:33:21 -0600

    adds resource representation in state. load_state returns a struct, not serde_json::Value

--- crates/cli_engine/src/lib.rs
+++ crates/cli_engine/src/lib.rs
@@ -1,23 +1,41 @@
-use dcl_language::{parse::SpannedDiagnostic, DclFile};
+use std::collections::HashMap;
+
+use dcl_language::{parse::{SpannedDiagnostic}, DclFile};
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Default)]
+pub struct StateFile {
+    /// reserved root field for later metadata, currently unused and values will show up as null
+    #[serde(default)]
+    pub dcl_metadata: serde_json::Value,
+    /// map of resources and their state representation.
+    /// note the key is the resource name, thus must be unique across all resources.
+    /// also note the ResourceInState also contains the resource name for convenience
+    pub resources: HashMap<String, ResourceInState>,
+}
 
 // TODO: need tp update DclFile definitions to preserve section line positions.
 // current reporting all errors on line 0 :/
 /// loads the state file and parses as a json object. if the state file doesnt exist, it will be
 /// created, and treated as an empty {} object
-pub fn load_state(dcl: &DclFile) -> Result<serde_json::Value, SpannedDiagnostic> {
+pub fn load_state(dcl: &DclFile) -> Result<StateFile, SpannedDiagnostic> {
     let state = dcl.state.as_ref().ok_or("no state file provided")
         .map_err(|e| SpannedDiagnostic::new(e, 0, 999))?;
     let file = match std::fs::OpenOptions::new().read(true).open(&state.file) {
         Ok(o) => o,
         Err(e) => match e.kind() {
             std::io::ErrorKind::NotFound => {
+                let default_statefile = StateFile::default();
+                let serialized = serde_json::to_string_pretty(&default_statefile).map_err(|e| {
+                    SpannedDiagnostic::new(format!("failed to serialize empty state file: {:?}", e), 0, 999)
+                })?;
                 // create a new empty state file:
-                std::fs::write(&state.file, "{}")
+                std::fs::write(&state.file, serialized)
                     .map_err(|e| SpannedDiagnostic::new(
                         format!("Failed to create empty state file '{}': {:?}", state.file, e),
                         0, 999)
                     )?;
-                return Ok(serde_json::Value::Object(Default::default()));
+                return Ok(default_statefile);
             },
             e => {
                 return Err(SpannedDiagnostic::new(
@@ -28,8 +46,26 @@ pub fn load_state(dcl: &DclFile) -> Result<serde_json::Value, SpannedDiagnostic>
         }
     };
     let buf = std::io::BufReader::new(file);
-    let out: serde_json::Value = serde_json::from_reader(buf)
+    let out: StateFile = serde_json::from_reader(buf)
         .map_err(|e| format!("Failed to read state file '{}' as json: {:?}", state.file, e))
         .map_err(|e| SpannedDiagnostic::new(e, 0, 999))?;
     Ok(out)
+}
+
+/// represents a resource that has successfully been created/updated in a state file
+#[derive(Deserialize, Serialize)]
+pub struct ResourceInState {
+    /// name of the resource. unique across all resources
+    pub resource_name: String,
+    /// name of the template this resource references. used to lookup
+    /// the template in case a deletion is necessary
+    pub template_name: String,
+    /// the entire input of this resource at the time
+    /// that it was passed to its template (ie: it does not have any json paths, all
+    /// json paths have been evaluated already and thus it is representable as a serde_json Value
+    /// instead of a json_with_positions Value). this is used
+    /// to compare against it's current input to see which fields, if any, have changed
+    pub last_input: serde_json::Value,
+    /// the entire output of this resource after it was done being processed by its template.
+    pub output: serde_json::Value,
 }
