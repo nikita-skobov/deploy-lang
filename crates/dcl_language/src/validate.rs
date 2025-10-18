4d5e0615a9c0db745cc23bbb7ef89d59ec97c13c
nikita-skobov <nskobov.websites@gmail.com>
2025-10-18 10:31:39 -0600

    adds validation for function references in resources

--- crates/dcl_language/src/validate.rs
+++ crates/dcl_language/src/validate.rs
@@ -1,6 +1,7 @@
 //! module for the various validations that should happen to a DclFile once it has been successfully parsed
 
 use jsonpath_rust::query::{state::State, Query};
+use str_at_line::StringAtLine;
 
 use crate::{parse::{resource::ResourceSection, SpannedDiagnostic}, DclFile};
 
@@ -42,6 +43,109 @@ pub fn resource_has_corresponding_template(resource: &ResourceSection, dcl: &Dcl
     }
 }
 
+/// returns Some(_) if the function name was found to be a function
+/// in the dcl file, None otherwise. this function will submit diagnostics as-needed.
+/// such that if it returns Some(_) there may be 0 or more diagnostics added, and the caller
+/// does not need to add more. if it returns None there are guaranteed to be no diagnostics added
+/// and the caller should continue checking if its a resource reference instead.
+pub fn resource_has_valid_function_reference(
+    resource: &ResourceSection,
+    dcl: &DclFile,
+    diagnostics: &mut Vec<SpannedDiagnostic>,
+    function_name: &str,
+    jpq: &jsonpath_rust::parser::model::JpQuery,
+    jpq_location: &StringAtLine,
+) -> Option<()> {
+    let _function = dcl.functions.iter().find(|x| x.function_name.as_str() == function_name)?;
+    // we know this reference is a function, perform validations that
+    // it only has 1 more segment
+    let last = match jpq.segments.last() {
+        Some(l) => l,
+        None => {
+            diagnostics.push(SpannedDiagnostic::from_str_at_line(jpq_location, format!("function reference '{}' must be followed by a resource name", jpq_location.as_str())));
+            return Some(());
+        }
+    };
+    // there should be 2 segments, but the function name is the first, and it was already removed from the segments.
+    if jpq.segments.len() != 1 {
+        diagnostics.push(
+            SpannedDiagnostic::from_str_at_line(
+                jpq_location,
+                format!(
+                    "function reference '{}' must only have 2 segments: the function name, followed by the resource name to be passed into the function",
+                    jpq_location.as_str()
+                )
+            )
+        );
+        return Some(());
+    }
+    // ensure it references a valid resource:
+    let mut function_arg_resource = match last {
+        jsonpath_rust::parser::model::Segment::Selector(selector) => {
+            match selector {
+                jsonpath_rust::parser::model::Selector::Name(s) => {
+                    s.clone()
+                }
+                _ => {
+                    diagnostics.push(
+                        SpannedDiagnostic::from_str_at_line(
+                            jpq_location,
+                            format!(
+                                "function reference '{}' 2nd segment must be a named selector",
+                                jpq_location.as_str()
+                            )
+                        )
+                    );
+                    return Some(());
+                }
+            }
+        },
+        _ => {
+            diagnostics.push(
+                SpannedDiagnostic::from_str_at_line(
+                    jpq_location,
+                    format!(
+                        "function reference '{}' 2nd segment must be a selector",
+                        jpq_location.as_str()
+                    )
+                )
+            );
+            return Some(());
+        }
+    };
+    unquote_bracketed_selector(&mut function_arg_resource);
+    if function_arg_resource == resource.resource_name.as_str() {
+        diagnostics.push(
+            SpannedDiagnostic::from_str_at_line(
+                jpq_location,
+                format!(
+                    "resource '{}' cannot call function '{}' with itself as the input",
+                    resource.resource_name,
+                    function_name,
+                )
+            )
+        );
+        return Some(());
+    }
+    match dcl.resources.iter().find(|x| x.resource_name.as_str() == function_arg_resource) {
+        Some(_) => {}
+        None => {
+            diagnostics.push(
+                SpannedDiagnostic::from_str_at_line(
+                    jpq_location,
+                    format!(
+                        "function reference '{}' attempts to call function '{}' with nonexistant resource '{}'",
+                        jpq_location.as_str(),
+                        function_name,
+                        function_arg_resource,
+                    )
+                )
+            );
+        }
+    }
+    Some(())
+}
+
 pub fn resource_has_valid_jsonpath_references(resource: &ResourceSection, dcl: &DclFile, diagnostics: &mut Vec<SpannedDiagnostic>) {
     let jsonpaths = resource.input.get_all_json_paths();
     for jp in jsonpaths {
@@ -58,8 +162,13 @@ pub fn resource_has_valid_jsonpath_references(resource: &ResourceSection, dcl: &
             continue;
         }
         let first_segment = jpq.segments.remove(0);
-        // must be a valid resource:
+        // must be a valid resource or function:
         let referenced_resource_name = first_segment.to_string();
+        // first, try to check if its a valid function:
+        if let Some(_) = resource_has_valid_function_reference(resource, dcl, diagnostics, &referenced_resource_name, &jpq, &jp) {
+            continue;
+        }
+        // if its not a function reference, then its a resource reference:
         let referenced_resource = match dcl.resources.iter().find(|r| r.resource_name.as_str() == referenced_resource_name.as_str()) {
             Some(r) => r,
             None => {
@@ -105,6 +214,18 @@ pub fn resource_has_valid_jsonpath_references(resource: &ResourceSection, dcl: &
         if jpq_res.is_nothing() {
             diagnostics.push(SpannedDiagnostic::from_str_at_line(jp.clone(), format!("resource '{}' json path query '{}' references a nonexistant input field of resource '{}'", resource.resource_name.as_str(), jp.as_str(), referenced_resource.resource_name.as_str())));
         }
+    }
+}
+
+pub fn unquote_bracketed_selector(s: &mut String) {
+    remove_bracketed_selector_quotes::<'"'>(s);
+    remove_bracketed_selector_quotes::<'\''>(s);
+}
+
+pub fn remove_bracketed_selector_quotes<const C: char>(s: &mut String) {
+    while s.starts_with(C) && s.ends_with(C) && s.len() > 1 {
+        s.remove(0);
+        s.pop();
     }
 }
 
@@ -367,5 +488,154 @@ resource nonexistant_template(my_resource)
         assert_eq!(source_errors, vec![
             "    {\n        \"a\n    }"
         ]);
+    }
+
+    #[test]
+    fn invalid_jsonpath_function_reference() {
+        let file = r#"
+template my_template
+  create
+    echo hi
+
+function javascript(myfunc)
+  console.log(1);
+
+resource my_template(other)
+    {"blah": $.myfunc}
+"#;
+        let diagnostics = parse_and_validate(file).expect_err("it should have validation errors");
+        let mut source_errors = extract_sources_with_messages(diagnostics, file);
+        assert_eq!(source_errors.len(), 1);
+        let first_err = source_errors.remove(0);
+        assert_eq!(first_err.0, "function reference '$.myfunc' must be followed by a resource name");
+        assert_eq!(first_err.1, "$.myfunc");
+    }
+
+    #[test]
+    fn jsonpath_function_references_nonexistant_resource() {
+        let file = r#"
+template my_template
+  create
+    echo hi
+
+function javascript(myfunc)
+  console.log(1);
+
+resource my_template(other)
+    {"blah": $.myfunc['this_resource_doesnt_exist']}
+"#;
+        let diagnostics = parse_and_validate(file).expect_err("it should have validation errors");
+        let mut source_errors = extract_sources_with_messages(diagnostics, file);
+        assert_eq!(source_errors.len(), 1);
+        let first_err = source_errors.remove(0);
+        assert_eq!(first_err.0, "function reference '$.myfunc['this_resource_doesnt_exist']' attempts to call function 'myfunc' with nonexistant resource 'this_resource_doesnt_exist'");
+        assert_eq!(first_err.1, "$.myfunc['this_resource_doesnt_exist']");
+    }
+
+    #[test]
+    fn jsonpath_function_ok() {
+        let file = r#"
+template my_template
+  create
+    echo hi
+
+function javascript(myfunc)
+  console.log(1);
+
+resource my_template(other)
+    {"blah": $.myfunc['this_resource_exists']}
+
+resource my_template(this_resource_exists)
+    {}
+"#;
+        let _dcl = parse_and_validate(file).expect("it should not error");
+    }
+
+    #[test]
+    fn jsonpath_function_nonquoted_reference_ok() {
+        let file = r#"
+template my_template
+  create
+    echo hi
+
+function javascript(myfunc)
+  console.log(1);
+
+resource my_template(other)
+    {"blah": $.myfunc.this_resource_exists}
+
+resource my_template(this_resource_exists)
+    {}
+"#;
+        let _dcl = parse_and_validate(file).expect("it should not error");
+    }
+
+    #[test]
+    fn jsonpath_function_only_two_segments() {
+        let file = r#"
+template my_template
+  create
+    echo hi
+
+function javascript(myfunc)
+  console.log(1);
+
+resource my_template(other)
+    {"blah": $.myfunc.this_resource_exists.canthaveanythinghere}
+
+resource my_template(this_resource_exists)
+    {}
+"#;
+        let diagnostics = parse_and_validate(file).expect_err("it should have diagnostic errors");
+        let mut source_errors = extract_sources_with_messages(diagnostics, file);
+        assert_eq!(source_errors.len(), 1);
+        let first_err = source_errors.remove(0);
+        assert_eq!(first_err.0, "function reference '$.myfunc.this_resource_exists.canthaveanythinghere' must only have 2 segments: the function name, followed by the resource name to be passed into the function");
+        assert_eq!(first_err.1, "$.myfunc.this_resource_exists.canthaveanythinghere");
+    }
+
+    #[test]
+    fn jsonpath_function_resource_must_be_named_selector() {
+        let file = r#"
+template my_template
+  create
+    echo hi
+
+function javascript(myfunc)
+  console.log(1);
+
+resource my_template(other)
+    {"blah": $.myfunc[0]}
+
+resource my_template(this_resource_exists)
+    {}
+"#;
+        let diagnostics = parse_and_validate(file).expect_err("it should have diagnostic errors");
+        let mut source_errors = extract_sources_with_messages(diagnostics, file);
+        assert_eq!(source_errors.len(), 1);
+        let first_err = source_errors.remove(0);
+        assert_eq!(first_err.0, "function reference '$.myfunc[0]' 2nd segment must be a named selector");
+        assert_eq!(first_err.1, "$.myfunc[0]");
+    }
+
+    #[test]
+    fn jsonpath_function_resource_must_not_be_same_as_caller() {
+        let file = r#"
+template my_template
+  create
+    echo hi
+
+function javascript(myfunc)
+  console.log(1);
+
+resource my_template(other)
+    {"blah": $.myfunc['other']}
+"#;
+        let diagnostics = parse_and_validate(file).expect_err("it should have diagnostic errors");
+        let mut source_errors = extract_sources_with_messages(diagnostics, file);
+        assert_eq!(source_errors.len(), 1);
+        let first_err = source_errors.remove(0);
+        assert_eq!(first_err.0, "resource 'other' cannot call function 'myfunc' with itself as the input");
+        assert_eq!(first_err.1, "$.myfunc['other']");
     }
 }
